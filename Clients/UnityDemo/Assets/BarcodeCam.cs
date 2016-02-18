07c5a2406173426a28d389af4caf2dd8947b37ee
Cedric Poon <cedric@poons.me>
2016-02-19 03:36:20 +0800

    Make ZXing.Net supports Unity iOS/OSX/Android
    Unity package under Clients/UnityDemo/
    * Supports ONLY decoding functions
    * All Encoding functions were removed

--- Clients/UnityDemo/Assets/BarcodeCam.cs
+++ Clients/UnityDemo/Assets/BarcodeCam.cs
@@ -1,4 +1,4 @@
-﻿/*
+/*
 * Copyright 2012 ZXing.Net authors
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
@@ -17,141 +17,130 @@
 using System.Threading;
 
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 using ZXing;
-using ZXing.QrCode;
+using System;
+using System.Collections;
 
 public class BarcodeCam : MonoBehaviour
 {
-   // Texture for encoding test
-   public Texture2D encoded;
+	private WebCamTexture camTexture;
+	private Thread qrThread;
 
-   private WebCamTexture camTexture;
-   private Thread qrThread;
+	private Color32[] c;
+	private int W, H;
 
-   private Color32[] c;
-   private int W, H;
-      
-   private Rect screenRect;
+	private Rect screenRect;
 
-   private bool isQuit;
+	private bool isQuit;
 
-   public string LastResult;
-   private bool shouldEncodeNow;
+	public string LastResult;
 
-   void OnGUI()
-   {
-      GUI.DrawTexture(screenRect, camTexture, ScaleMode.ScaleToFit);
-   }
+	public bool hasResult;
 
-   void OnEnable()
-   {
-      if (camTexture != null)
-      {
-         camTexture.Play();
-         W = camTexture.width;
-         H = camTexture.height;
-      }
-   }
+	void OnGUI ()
+	{
+#if UNITY_IOS
+		// Flip GUI layout
+		GUIUtility.ScaleAroundPivot(new Vector2(1, -1), new Vector2(
+			screenRect.xMin + screenRect.width * 0.5f,
+			screenRect.yMin + screenRect.height * 0.5f
+		));
+#endif
+		GUI.DrawTexture (screenRect, camTexture, ScaleMode.ScaleToFit);
+	}
 
-   void OnDisable()
-   {
-      if (camTexture != null)
-      {
-         camTexture.Pause();
-      }
-   }
+	void OnEnable ()
+	{
+		if (camTexture != null) {
+			camTexture.Play ();
+			W = camTexture.width;
+			H = camTexture.height;
+		}
+	}
 
-   void OnDestroy()
-   {
-      qrThread.Abort();
-      camTexture.Stop();
-   }
+	void OnDisable ()
+	{
+		if (camTexture != null) {
+			camTexture.Pause ();
+		}
+	}
 
-   // It's better to stop the thread by itself rather than abort it.
-   void OnApplicationQuit()
-   {
-      isQuit = true;
-   }
+	void OnDestroy ()
+	{
+		qrThread.Abort ();
+		camTexture.Stop ();
+	}
 
-   void Start()
-   {
-      encoded = new Texture2D(256, 256);
-      LastResult = "http://www.google.com";
-      shouldEncodeNow = true;
+	// It's better to stop the thread by itself rather than abort it.
+	void OnApplicationQuit ()
+	{
+		isQuit = true;
+	}
 
-      screenRect = new Rect(0, 0, Screen.width, Screen.height);
+	void Start ()
+	{
+		LastResult = "http://www.google.com";
 
-      camTexture = new WebCamTexture();
-      camTexture.requestedHeight = 480;
-      camTexture.requestedWidth = 640;
-      OnEnable();
+		screenRect = new Rect (0, 0, Screen.width, Screen.height);
+		camTexture = new WebCamTexture ();
+		camTexture.requestedHeight = 480;
+		camTexture.requestedWidth = 640;
+		OnEnable ();
 
-      qrThread = new Thread(DecodeQR);
-      qrThread.Start();
-   }
+		qrThread = new Thread (DecodeQR);
 
-   void Update()
-   {
-      if (c == null)
-      {
-         c = camTexture.GetPixels32();
-      }
+		StartCoroutine (WaitUntilCamTextureReady ());
+	}
 
-      // encode the last found
-      var textForEncoding = LastResult;
-      if (shouldEncodeNow && 
-          textForEncoding != null)
-      {
-         var color32 = Encode(textForEncoding, encoded.width, encoded.height);
-         encoded.SetPixels32(color32);
-         encoded.Apply();
-         shouldEncodeNow = false;
-      }
-   }
+	void Update ()
+	{
+		if (c == null) {
+			c = camTexture.GetPixels32 ();
+		}
+	}
 
-   void DecodeQR()
-   {
-      // create a reader with a custom luminance source
-      var barcodeReader = new BarcodeReader {AutoRotate = false, TryHarder = false};
+	IEnumerator WaitUntilCamTextureReady ()
+	{
+		// Wait till camTexture ready
+		yield return new WaitUntil (() =>
+			camTexture.width >= camTexture.requestedWidth &&
+		camTexture.height >= camTexture.requestedHeight &&
+		camTexture.GetPixels32 ().Length == camTexture.width * camTexture.height
+		);
+		// Clear old pixel32[]
+		c = null;
+		// Reset variable W H
+		OnEnable ();
+		qrThread.Start ();
+	}
 
-      while (true)
-      {
-         if (isQuit)
-            break;
+	void DecodeQR ()
+	{
+		// create a reader with a custom luminance source
+		var barcodeReader = new BarcodeReader { AutoRotate = false, TryHarder = false };
 
-         try
-         {
-            // decode the current frame
-            var result = barcodeReader.Decode(c, W, H);
-            if (result != null)
-            {
-               LastResult = result.Text;
-               shouldEncodeNow = true;
-               print(result.Text);
-            }
+		while (true) {
+			if (isQuit)
+				break;
 
-            // Sleep a little bit and set the signal to get the next frame
-            Thread.Sleep(200);
-            c = null;
-         }
-         catch
-         {
-         }
-      }
-   }
+			try {
+				// decode the current frame
+				var result = barcodeReader.Decode (c, W, H);
+				if (result != null) {
+					LastResult = result.Text;
+					Debug.Log (result.Text);
 
-   private static Color32[] Encode(string textForEncoding, int width, int height)
-   {
-      var writer = new BarcodeWriter
-      {
-         Format = BarcodeFormat.QR_CODE,
-         Options = new QrCodeEncodingOptions
-         {
-            Height = height,
-            Width = width
-         }
-      };
-      return writer.Write(textForEncoding);
-   }
+					// Indicate loaded QR
+					hasResult = true;
+				}
+
+				// Sleep a little bit and set the signal to get the next frame
+				Thread.Sleep (200);
+				c = null;
+			} catch {
+			}
+		}
+	}
 }
